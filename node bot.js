ea05c196b451e57e08e307cb8c20dc6067c3fa74
heilok0808bot <leungheilok.bot@gmail.com>
2017-07-10 21:09:07 +0800

    Create node bot.js

---
+++ node bot.js
@@ -0,0 +1,9 @@
+var bot = require("discord-music-bot");
+
+var serverName = "中二病病院";
+var textChannelName = "commands";
+var voiceChannelName = "Music & Chill Channle";
+var aliasesFile = "aliases";
+var botToken = "MzMyOTcyMDUyMTIwMDc2Mjg5.DEUIRQ.tOjv5kX05XN1b2cGhxpSafX_9qM";
+
+bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
